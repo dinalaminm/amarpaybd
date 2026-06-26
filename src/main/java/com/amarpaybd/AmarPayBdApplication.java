677c43a4f72cf136cfd428372c8065d5dcda1aa3
dinalaminm <mdalaminal203@gmail.com>
2026-06-26 08:36:33 +0600

    Add files via upload

---
+++ src/main/java/com/amarpaybd/AmarPayBdApplication.java
@@ -0,0 +1,11 @@
+package com.amarpaybd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmarPayBdApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AmarPayBdApplication.class, args);
+    }
+}
